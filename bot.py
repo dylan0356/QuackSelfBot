df8af60859d5a38638c94d1cc14db38b8f0f2420
Dylan <33008329+dylan0356@users.noreply.github.com>
2023-08-30 16:57:48 -0700

    init

--- bot.py
+++ bot.py
@@ -1,1 +1,35 @@
 
+import discord
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        # only respond to ourselves
+        if message.author != self.user:
+            return
+
+        if message.content == 'ping':
+            await message.channel.send('pong')
+
+
+token_list = open("token.txt", "r").readlines()
+
+for token in token_list:
+    client = MyClient()
+    
+    try:
+        client.run(token)
+    except discord.errors.LoginFailure:
+        print("Token " + token + " is invalid")
+        continue
+    
+
+    if client.is_closed():
+        print("Token " + token + " is invalid")
+
+    else:
+        print("Token " + token + " is valid")
+
+        await client.close()
